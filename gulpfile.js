6c3c21394041e6500086ad60e38b68b49598e795
Alexander Krasnovskyy <alexander.kaj@gmail.com>
2021-10-20 18:50:08 +0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const {  src, dest, watch, series } = require("gulp");
+const sourcemaps = require("gulp-sourcemaps");
+const sass = require('gulp-sass')(require('sass'));
+
+function buildStyles() {
+  return src("./scss/**/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(sourcemaps.write("./maps"))
+    .pipe(dest("./css"));
+}
+
+exports.default = function() {
+  watch('./scss/**/*.scss', buildStyles);
+};
+
+exports.buildStyles = buildStyles;
